ca872e7f942010be1a1f6719330a56c66130e8df
Floias <irina.butcher@gmail.com>
2023-10-22 19:33:47 +0500

    исправления

--- js/modules/drawingThumbnails.ts
+++ js/modules/drawingThumbnails.ts
@@ -1,17 +1,17 @@
 import {createPhotos} from './createPhotosArray';
 
-const template: HTMLTemplateElement = <HTMLTemplateElement>document.querySelector('#picture');
-const templatePicture: HTMLElement = <HTMLElement>template.content.querySelector('.picture');
-const picturesList: HTMLElement = <HTMLElement>document.querySelector('.pictures');
+const template: HTMLTemplateElement = document.querySelector('#picture');
+const templatePicture: HTMLElement = template.content.querySelector('.picture');
+const picturesList: HTMLElement = document.querySelector('.pictures');
 const picturesListFragment: DocumentFragment = document.createDocumentFragment();
 
 const drawingThumbnail = createPhotos;
 
 drawingThumbnail.forEach(({url, description, likes, comments}) => {
   const pictureElement: HTMLElement = <HTMLElement>templatePicture.cloneNode(true);
-  const pictureImg: HTMLImageElement = <HTMLImageElement>pictureElement.querySelector('.picture__img');
-  const pictureLikes: HTMLSpanElement = <HTMLSpanElement>pictureElement.querySelector('.picture__likes');
-  const pictureComments: HTMLSpanElement = <HTMLSpanElement>pictureElement.querySelector('.picture__comments');
+  const pictureImg: HTMLImageElement = pictureElement.querySelector('.picture__img');
+  const pictureLikes: HTMLSpanElement = pictureElement.querySelector('.picture__likes');
+  const pictureComments: HTMLSpanElement = pictureElement.querySelector('.picture__comments');
 
   pictureImg.src = url;
   pictureImg.alt = description;
